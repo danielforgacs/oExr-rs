482813863e2e39aeb2cb2f08448d7e9a4d32dcd5
df@ <@>
2023-08-09 23:48:31 +0100

    writing serialised exr struct to file ok.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::io::prelude::*;
-use std::fs::read;
+use std::fs::write;
 
 
 const MAGIC_NUMBER: [u8;4] = [0x76, 0x2f, 0x31, 0x01];
@@ -10,7 +9,24 @@ struct Exr {
     version: [u8;4],
 }
 
+impl Exr {
+    fn new() -> Self {
+        Self {
+            magic_number: MAGIC_NUMBER,
+            version: VERSION,
+        }
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+        buffer.extend(self.magic_number);
+        buffer.extend(self.version);
+        buffer
+    }
+}
+
 fn main() {
-    let data = read("test_images/exr_4x4_16f_rgb_uncompressed_white.exr");
-    dbg!(&data);
+    let exr = Exr::new();
+    let data = exr.serialize();
+    write("test_result.exr", data).unwrap();
 }
