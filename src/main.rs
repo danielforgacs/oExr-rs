8b473a4c6382a9a2f8fc27f219439cebbea2d0fd
df@ <@>
2023-08-09 23:08:57 +0100

    exr struct update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::io::prelude::*;
+use std::fs::read;
+
+
 const MAGIC_NUMBER: [u8;4] = [0x76, 0x2f, 0x31, 0x01];
+const VERSION: [u8; 4] = [0x02, 0x00, 0x00, 0x00];
 
 struct Exr {
     magic_number: [u8;4],
@@ -6,4 +11,6 @@ struct Exr {
 }
 
 fn main() {
+    let data = read("test_images/exr_4x4_16f_rgb_uncompressed_white.exr");
+    dbg!(&data);
 }
