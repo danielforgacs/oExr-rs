a9e5d331cf34693e12b086e7e36ecf2275b1814b
df@ <@>
2023-08-10 20:41:52 +0100

    __wip__

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::write;
+use std::collections::HashMap;
 
 
 const MAGIC_NUMBER: [u8;4] = [0x76, 0x2f, 0x31, 0x01];
@@ -7,13 +8,30 @@ const VERSION: [u8; 4] = [0x02, 0x00, 0x00, 0x00];
 struct Exr {
     magic_number: [u8;4],
     version: [u8;4],
+    header: Vec<HashMap<String, Header>>,
+}
+
+struct Header {
+    name: String,
+    size:  String,
 }
 
 impl Exr {
     fn new() -> Self {
+        let header = HashMap::new();
+        header.insert(
+            "channels".to_string(),
+            Header {
+
+            }
+        )
         Self {
             magic_number: MAGIC_NUMBER,
             version: VERSION,
+            header: Header {
+                name: "channels".to_string(),
+                size: "chlist".to_string(),
+            }
         }
     }
 
