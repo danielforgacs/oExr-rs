43f14864f065b50d04feab91b00755dff740b8b6
df@ <@>
2023-08-11 20:33:37 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::write;
+use std::fs::{write, read};
 
 const MAGIC_NUMBER: [u8;4] = [0x76, 0x2f, 0x31, 0x01];
 const VERSION: [u8; 4] = [0x02, 0x00, 0x00, 0x00];
@@ -16,6 +16,23 @@ impl Exr {
         }
     }
 
+    fn deserialize(mut data: Vec<u8>) -> Exr {
+        let magic_number: [u8; 4] = data
+            .drain(..4)
+            .collect::<Vec<u8>>()
+            .try_into()
+            .unwrap();
+        let version: [u8; 4] = data
+            .drain(..4)
+            .collect::<Vec<u8>>()
+            .try_into()
+            .unwrap();
+        Self {
+            magic_number,
+            version,
+        }
+    }
+
     fn serialize(&self) -> Vec<u8> {
         let mut buffer = Vec::new();
         buffer.extend(self.magic_number);
@@ -25,6 +42,8 @@ impl Exr {
 }
 
 fn main() {
+    let data = read("example_exr_from_docs.exr").unwrap();
+    let example_exr = Exr::deserialize(data);
     let exr = Exr::new();
     let data = exr.serialize();
     write("test_result.exr", data).unwrap();
