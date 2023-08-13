d23adf244c4c50c7d1b686088fa7719cc10dfeb3
df@ <@>
2023-08-13 01:06:34 +0100

    ok

--- src/structs/exr.rs
+++ src/structs/exr.rs
@@ -4,6 +4,8 @@ const VERSION: [u8; 4] = [0x02, 0x00, 0x00, 0x00];
 pub struct Exr {
     magic_number: [u8;4],
     version: [u8;4],
+    header: Vec<u8>,
+    offset_tables: Vec<u8>,
     leftover_bytes: Vec<u8>,
 }
 
@@ -25,9 +27,21 @@ impl Exr {
         if version != VERSION {
             panic!("Version does not match.");
         };
+        let header = data
+            .drain(..288)
+            .collect::<Vec<u8>>()
+            .try_into()
+            .unwrap();
+        let offset_tables = data
+            .drain(..25)
+            .collect::<Vec<u8>>()
+            .try_into()
+            .unwrap();
         Self {
             magic_number,
             version,
+            header,
+            offset_tables,
             leftover_bytes: data,
         }
     }
@@ -36,6 +50,8 @@ impl Exr {
         let mut buffer = Vec::new();
         buffer.extend(self.magic_number);
         buffer.extend(self.version);
+        buffer.extend(self.header.clone());
+        buffer.extend(self.offset_tables.clone());
         buffer.extend(self.leftover_bytes.clone());
         buffer
     }
