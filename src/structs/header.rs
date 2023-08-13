4f5a18fb24352c9c4c96963ff194da1026a57f04
df@ <@>
2023-08-13 12:27:59 +0100

    refactor

--- src/structs/header.rs
+++ src/structs/header.rs
@@ -6,36 +6,39 @@ pub struct Header {
 
 impl Header {
     pub fn deserialize(data: &mut Vec<u8>) -> Self {
-        let mut raw_data: Vec<u8> = Vec::new();
+        let mut header_bytes: Vec<u8> = Vec::new();
         loop {
+            // This loop is extracting attr name, attr type and attr data tuples.
+            // The last data must (?) be "screenWindowWidth". Data after that
+            // belongs to the offset tables
             let name = utils::parse_until_null(data);
-            raw_data.extend(name.bytes().clone());
+            header_bytes.extend(name.bytes().clone());
             data.drain(..1);
-            raw_data.push(0);
+            header_bytes.push(0);
             println!("attribute: {}", &name);
 
             let attr_type = utils::parse_until_null(data);
-            raw_data.extend(attr_type.bytes().clone());
+            header_bytes.extend(attr_type.bytes().clone());
             data.drain(..1);
-            raw_data.push(0);
+            header_bytes.push(0);
             println!("  attribute type: {}", &attr_type);
 
             let attr_bytes_count: Vec<u8> = data.drain(..4).collect();
-            raw_data.extend(attr_bytes_count.clone());
+            header_bytes.extend(attr_bytes_count.clone());
             let attr_bytes_count = u32::from_le_bytes(attr_bytes_count.try_into().unwrap());
             println!("  attribute len: {}", &attr_bytes_count);
 
             let attr_data: Vec<u8> = data.drain(..attr_bytes_count as usize).collect();
-            raw_data.extend(attr_data.clone());
+            header_bytes.extend(attr_data.clone());
             println!("  attribute data: {:02X?}", &attr_data);
 
             if &name == "screenWindowWidth" {
                 data.push(0);
-                raw_data.push(0);
+                header_bytes.push(0);
                 break;
             }
         }
-        Self { data: raw_data }
+        Self { data: header_bytes }
     }
 
     pub fn serialize(&self) -> Vec<u8> {
