b54a5666f3692aaf07c7cf8086e3e5f11345cd2a
df@ <@>
2023-08-15 00:37:28 +0100

    finding the format verison & the multi part bit ok.

--- src/exr.rs
+++ src/exr.rs
@@ -15,8 +15,13 @@ impl Exr {
         if magic_num != MAGIC_NUMBER_U32 {
             panic!("The magic number is wrong!");
         }
-        let version_field = u32::from_le_bytes(data[..4].to_owned().try_into().unwrap());
-        println!(":: version field: {}, {:#034b}", version_field, version_field);
+        let version_field = u32::from_le_bytes(
+            data.drain(..4)
+            .collect::<Vec<u8>>()
+            .to_owned()
+            .try_into()
+            .unwrap()
+        );
         let format_version_number = version_field & 0b_00000000_00000000_00000000_11111111;
         println!(":: format version number: {}", format_version_number);
         let multipart_bit = (version_field & 0b_00000000_00000000_00010000_00000000) >> 12;
@@ -30,6 +35,8 @@ impl Exr {
 
     pub fn serialize(&self) -> Vec<u8> {
         let mut data = MAGIC_NUMBER_U32.to_le_bytes().to_vec();
+        let version_field = self.format_version_number | (self.multipart_bit << 12);
+        data.extend(version_field.to_le_bytes());
         data.extend(self.left_over_bytes.clone());
         data
     }
