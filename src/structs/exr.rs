23eee898713a104ee93d64e87ca0f0beb010cb67
df@ <.@.>
2023-08-23 23:14:43 +0100

    added version field.

--- src/structs/exr.rs
+++ src/structs/exr.rs
@@ -1,19 +1,23 @@
 use crate::consts;
+use super::vfield;
 
 pub struct Exr {
     magic_number: [u8; 4],
+    version_field: vfield::VersionField,
 }
 
 impl Exr {
     pub fn new() -> Self {
         Self {
             magic_number: consts::MAGIC_NUMBER,
+            version_field: vfield::VersionField::new(),
         }
     }
 
     pub fn serialize(&self) -> Vec<u8> {
         let mut exr_bytes = vec![];
         exr_bytes.extend(self.magic_number);
+        exr_bytes.extend(self.version_field.serialize());
         exr_bytes
     }
 }
@@ -25,7 +29,10 @@ mod tests {
     #[test]
     fn test_exr() {
         let exr = Exr::new();
-        let expected = vec![0x76, 0x2f, 0x31, 0x01];
+        let expected = vec![
+            0x76, 0x2f, 0x31, 0x01,
+            0x02, 0x00, 0x00, 0x00,
+        ];
         assert_eq!(exr.serialize(), expected);
     }
 }
