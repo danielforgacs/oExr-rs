e14b80c7e7a2d256a99a9f0d1dac1de195bf246a
df@ <.@.>
2023-09-05 22:42:33 +0100

    started writing attrs - channel

--- src/structs/exr.rs
+++ src/structs/exr.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 use crate::consts;
 use super::vfield;
 use super::chan;
-use super::chan::{Channel, ChannelType};
+// use super::chan::{Channel, ChannelType};
 
 pub struct Exr {
     magic_number: [u8; 4],
@@ -23,10 +23,18 @@ impl Exr {
         }
     }
 
+    pub fn add_channel(&mut self, chan: chan::Channel) {
+        self.channels.push(chan);
+    }
+
     pub fn serialize(&self) -> Vec<u8> {
         let mut exr_bytes = Vec::new();
         exr_bytes.extend(self.magic_number);
         exr_bytes.extend(self.version_field.serialize());
+        exr_bytes.extend("channels".as_bytes());
+        exr_bytes.push(0);
+        exr_bytes.extend("chlist".as_bytes());
+        exr_bytes.push(0);
         {
             for y in 0..self.res_y {
                 let mut scan_line = Vec::new();
@@ -50,30 +58,40 @@ mod tests {
 
     #[test]
     fn test_exr() {
-        let mut exr = Exr::new(4, 3);
-
-        let pixel_data = vec![
+        let pixel_data_g = vec![
             f16::from_le_bytes([0x00, 0x00]), f16::from_le_bytes([0x54, 0x29]), f16::from_le_bytes([0xd5, 0x35]), f16::from_le_bytes([0xe8, 0x2d]),
             f16::from_le_bytes([0x37, 0x38]), f16::from_le_bytes([0x76, 0x33]), f16::from_le_bytes([0x74, 0x3b]), f16::from_le_bytes([0x73, 0x38]),
             f16::from_le_bytes([0x23, 0x3a]), f16::from_le_bytes([0x0a, 0x34]), f16::from_le_bytes([0x02, 0x3b]), f16::from_le_bytes([0x5d, 0x3b]),
         ];
-        let chan_g = Channel::new("G", ChannelType::Half(pixel_data));
-        exr.channels.push(chan_g);
-
-        let pixel_data = vec![
+        let pixel_data_z = vec![
             f32::from_le_bytes([0x5c, 0x28, 0x81, 0x3a]), f32::from_le_bytes([0xcf, 0xe1, 0x34, 0x3e]), f32::from_le_bytes([0x8b, 0x0b, 0xbb, 0x3d]), f32::from_le_bytes([0x89, 0x74, 0xf9, 0x3e]),
             f32::from_le_bytes([0x7f, 0xab, 0xe8, 0x3e]), f32::from_le_bytes([0x8a, 0xcf, 0x54, 0x3f]), f32::from_le_bytes([0x5b, 0x6c, 0x11, 0x3f]), f32::from_le_bytes([0x20, 0x35, 0x50, 0x3d]),
             f32::from_le_bytes([0x38, 0xf3, 0x9a, 0x3c]), f32::from_le_bytes([0x4d, 0xad, 0x98, 0x3e]), f32::from_le_bytes([0x1c, 0x14, 0x08, 0x3f]), f32::from_le_bytes([0x4c, 0xf3, 0x03, 0x3f]),
         ];
-        let chan_z = Channel::new("Z", ChannelType::FLoat(pixel_data));
-        exr.channels.push(chan_z);
 
+        let mut exr = Exr::new(4, 3);
+        let chan_g = chan::Channel::new("G", chan::ChannelType::Half(pixel_data_g));
+        exr.add_channel(chan_g);
+        let chan_z = chan::Channel::new("Z", chan::ChannelType::FLoat(pixel_data_z));
+        exr.add_channel(chan_z);
 
         let expected = vec![
             // magic num
             0x76, 0x2f, 0x31, 0x01,
             // version bytes
             0x02, 0x00, 0x00, 0x00,
+
+            // Header attributes
+                // "channels"
+                0x63, 0x68, 0x61, 0x6e, 0x6e, 0x65, 0x6c, 0x73,
+                // null byte
+                0x00,
+                // "chlist"
+                0x63, 0x68, 0x6c, 0x69, 0x73, 0x74,
+                // null byte
+                0x00,
+
+
 
 
             // scanline 0 pixel values
