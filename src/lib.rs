3e89ee1cc6d12c8492e4f596e0553c03c7fd6144
df@ <.@.>
2023-09-15 18:58:32 +0100

    set up lib & tests.

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub fn it_works_adding_two_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
---
+++ tests/lib.rs
@@ -0,0 +1,12 @@
+use oexr::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_it_works() {
+        let result = it_works_adding_two_numbers(3, 4);
+        assert_eq!(result, 3 + 4);
+    }
+}
